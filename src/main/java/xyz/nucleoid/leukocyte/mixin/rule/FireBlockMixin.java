56e7488cfd3f0d9d2c38aa1c998aa3da0ecc4d75
PotatoPresident <potatoboy9999@outlook.com>
2021-03-01 19:52:24 -0800

    Fixed FIRE_TICK ProtectionRule to default to vanilla gamerule Updated README.md

--- src/main/java/xyz/nucleoid/leukocyte/mixin/rule/FireBlockMixin.java
+++ src/main/java/xyz/nucleoid/leukocyte/mixin/rule/FireBlockMixin.java
@@ -4,26 +4,31 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FireBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.nucleoid.leukocyte.Leukocyte;
 import xyz.nucleoid.leukocyte.RuleQuery;
 import xyz.nucleoid.leukocyte.rule.ProtectionRule;
+import xyz.nucleoid.leukocyte.rule.RuleResult;
 
 import java.util.Random;
 
 @Mixin(FireBlock.class)
 public class FireBlockMixin {
-    @Inject(method = "scheduledTick", at = @At("HEAD"), cancellable = true)
-    private void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+    @Redirect(method = "scheduledTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$Key;)Z"))
+    private boolean test(GameRules gameRules, GameRules.Key<GameRules.BooleanRule> rule, BlockState state, ServerWorld world, BlockPos pos, Random random) {
         Leukocyte leukocyte = Leukocyte.byWorld(world);
-        if (leukocyte != null) {
-            RuleQuery query = RuleQuery.at(world, pos);
-            if (leukocyte.denies(query, ProtectionRule.FIRE_TICK)) {
-                ci.cancel();
-            }
+        RuleQuery query = RuleQuery.at(world, pos);
+
+        RuleResult result = leukocyte.test(query, ProtectionRule.FIRE_TICK);
+        if (result.isDefinitive()) {
+            return result == RuleResult.ALLOW;
         }
+
+        return gameRules.getBoolean(GameRules.DO_FIRE_TICK);
     }
 }
